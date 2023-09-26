a910c81049541a35af22d1822982e8014c4fb9d8
acsantanamx <60712262+acsantanamx@users.noreply.github.com>
2023-09-26 15:44:38 -0600

    Update invalid-integer-to-convert-exception.ts

--- src/exceptions/invalid-integer-to-convert-exception.ts
+++ src/exceptions/invalid-integer-to-convert-exception.ts
@@ -1,30 +1,24 @@
 export class InvalidIntegerToConvertException extends Error {
-    private readonly value: number;
+  constructor(message, value) {
+    super(message)
+    this.value = value
+  }
 
-    private constructor(message: string, value: number) {
-        super(message);
-        this.value = value;
-    }
+  static lowerThanZero(value) {
+    return new InvalidIntegerToConvertException(
+      "The integer serial number is lower than zero",
+      value
+    )
+  }
 
-    public static lowerThanZero(
-        value: number
-    ): InvalidIntegerToConvertException {
-        return new InvalidIntegerToConvertException(
-            'The integer serial number is lower than zero',
-            value
-        );
-    }
+  static greaterThanMaximum(value) {
+    return new InvalidIntegerToConvertException(
+      "The integer serial number is greater than maximum value",
+      value
+    )
+  }
 
-    public static greaterThanMaximum(
-        value: number
-    ): InvalidIntegerToConvertException {
-        return new InvalidIntegerToConvertException(
-            'The integer serial number is greater than maximum value',
-            value
-        );
-    }
-
-    public getValue(): number {
-        return this.value;
-    }
+  getValue() {
+    return this.value
+  }
 }
