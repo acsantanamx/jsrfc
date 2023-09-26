d673a02e5179bd4b065cf4a1651ffe76929e920a
acsantanamx <60712262+acsantanamx@users.noreply.github.com>
2023-09-26 15:45:07 -0600

    Update invalid-expression-to-parse-exception.ts

--- src/exceptions/invalid-expression-to-parse-exception.ts
+++ src/exceptions/invalid-expression-to-parse-exception.ts
@@ -1,26 +1,24 @@
 export class InvalidExpressionToParseException extends Error {
-    private readonly rfc: string;
+  constructor(message, rfc) {
+    super(message)
+    this.rfc = rfc
+  }
 
-    private constructor(message: string, rfc: string) {
-        super(message);
-        this.rfc = rfc;
-    }
+  static invalidParts(rfc) {
+    return new InvalidExpressionToParseException(
+      "The RFC expression does not contain the valid parts",
+      rfc
+    )
+  }
 
-    public static invalidParts(rfc: string): InvalidExpressionToParseException {
-        return new InvalidExpressionToParseException(
-            'The RFC expression does not contain the valid parts',
-            rfc
-        );
-    }
+  static invalidDate(rfc) {
+    return new InvalidExpressionToParseException(
+      "The RFC expression does not contain a valid date",
+      rfc
+    )
+  }
 
-    public static invalidDate(rfc: string): InvalidExpressionToParseException {
-        return new InvalidExpressionToParseException(
-            'The RFC expression does not contain a valid date',
-            rfc
-        );
-    }
-
-    public getRfc(): string {
-        return this.rfc;
-    }
+  getRfc() {
+    return this.rfc
+  }
 }
